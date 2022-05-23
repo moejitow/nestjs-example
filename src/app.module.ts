ee914dd4855c705b5a31412d37316cbf3741a91f
Julius Kiefer <juliuskiefer@web.de>
2022-05-23 18:19:35 +0200

    added typeorm for mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
