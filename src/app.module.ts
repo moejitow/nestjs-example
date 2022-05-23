5a469be53eba9902eec8f24735e47bc4a234866f
Julius Kiefer <juliuskiefer@web.de>
2022-05-23 21:51:59 +0200

    added entity and service for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
